83e0096b19335f0cc90c4a3b23443c8d72d2ba11
dyike <yuanfeng634@gmail.com>
2025-09-03 23:15:55 +0800

    fix conditional logic

--- internal/graph/conditional_logic.go
+++ internal/graph/conditional_logic.go
@@ -2,10 +2,26 @@ package graph
 
 import (
 	"context"
+	"strings"
+
+	"github.com/cloudwego/eino/compose"
+	"github.com/dyike/CortexGo/consts"
+	"github.com/dyike/CortexGo/internal/models"
 )
 
 func ShouldContinueDebate(ctx context.Context, input string) (next string, err error) {
-	return "", nil
+	next = consts.BullResearcher
+	_ = compose.ProcessState[*models.TradingState](ctx, func(_ context.Context, state *models.TradingState) error {
+		if state.InvestmentDebateState.Count >= 2 {
+			next = consts.ResearchManager
+		}
+		curResp := state.InvestmentDebateState.CurrentResponse
+		if strings.HasPrefix(curResp, "Bull") {
+			next = consts.BearResearcher
+		}
+		return nil
+	})
+	return next, nil
 }
 
 func ShouldContinueRiskAnalysis(ctx context.Context, input string) (next string, err error) {
